b52cf96ebe60797e8a6c809d1da0cb32522756b1
Rishima Borah <138853322+RishimaaB@users.noreply.github.com>
2023-07-18 22:31:51 +0530

    Update jRCT_R_scripts.R

--- jRCT_Github/jRCT_R_scripts.R
+++ jRCT_Github/jRCT_R_scripts.R
@@ -240,3 +240,18 @@ for (i in seq_along(ids)) {
     }
   }
 }
+
+
+#*****************************************************************Script - 4*************************************************************************************************
+#Storing all the files in a local SQLite database 
+
+libraries = c("tidyverse", "stringr",  "rvest", "XML", "purrr", "data.table", "DBI", "httr")
+lapply(libraries, require, character.only = TRUE)
+
+mydb <- dbConnect(RSQLite::SQLite(), "JRCT.sqlite")
+file <- read.csv(file.choose())
+dbWriteTable(mydb, "file", file, append = TRUE)
+
+
+
+#*******************************************************************The End*****************************************************************************************************
